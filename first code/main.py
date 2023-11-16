20303bf66add89cf4e6165bef7338010dade06f0
Jenny Ann Guyong <145944211+jatguyong@users.noreply.github.com>
2023-11-16 22:59:50 +0800

    Update main.py

--- first code/main.py
+++ first code/main.py
@@ -1,34 +1,42 @@
 import time
 import random
 from scene_1 import scene1
-
+from scene_2 import scene2
 
 def startGame():
-    print("""
-    -->> MENU <<--
-    [1] Start Game
-    [2] Credits
-    [3] Quit""")
+    print()
+    print("##########################")
+    print("# Welcome to The Escape! #")
+    print("##########################")
+    print("#  -->>> M E N U <<<--   #")
+    print("#    [1]  Start Game     #")
+    print("#    [2]  Credits        #")
+    print("#    [3]  Quit           #")
+    print("##########################")
 
     print()
 
     user_input = input("Enter an option: ")
 
     if user_input == '1':
+        sceneList = (scene1, scene2) 
+        #this is a list of the different scenes, which are functions
 
-        sceneList = (scene1) #list of the different scenes excluding the boss fight
-        random.choice(sceneList) #randomly choose from the scenes in sceneList
-        
+        random.choice(sceneList)()
+        #we are randomly selecting a choice from any of the functions inside the sceneList
+
     elif user_input == '2':
         credits()
     elif user_input == '3':
-        global isPlaying #makes isPlaying accessible inside this function
+        global isPlaying 
+        #global makes isPlaying accessible inside this function
 
         print("\nThank you for playing The Escape!")
         print("Quote dito huehuehue")
 
         isPlaying = False
-    else: #if player entered a number other than 1, 2, or 3
+    else: 
+        #if player entered a number other than 1, 2, or 3
         print("\nOption Invalid!")
 
 
@@ -49,7 +57,7 @@ def credits():
     time.sleep(1)
     print("GIRLY POP!")
     print()
-
+    
     time.sleep(1)
 
     print("Lucci Ania Luisse Fagela")
@@ -60,14 +68,8 @@ def credits():
     time.sleep(1)
     print("Princess Joy Vidad")
 
-
-
-#while isPlaying is True, the program or the game will continue to run and will be terminated when it is set to False perhaps when player wants to quit or when they lose?
-#btw should we give them the option to quit while in game?
+#since isPlaying is set to True, it will execute the while loop and will continue to do so until it is set to False
 
 isPlaying = True
 while isPlaying:
-    print("\n========================================================================================================================\n")
-    print("Welcome to The Escape!")
     startGame()
-
